f0e695a959b1a57526039aceb865a8c0f23232b1
todorokimaru <hiro1616be@gmail.com>
2016-08-18 11:07:12 +0900

    とりあえず作成

--- Xml_Loader/Program.cs
+++ Xml_Loader/Program.cs
@@ -4,12 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Xml;
+
 namespace Xml_Loader
 {
     class Program
     {
         static void Main(string[] args)
         {
+            XmlDocument doc = new XmlDocument();
+
+            doc.Load("FG-GML-5437-70-dem10b-20090201.xml");
+
+            XmlNode root = doc.DocumentElement;//<samle>
+
+            Console.WriteLine(root.Name);
+
+            XmlNode A = root.FirstChild;//最初の子ノードを取得
+
+            Console.WriteLine(A.Name);
+            //attribute を取得するときはXmlNodeをXmlElementに変換する。
+            Console.WriteLine(((XmlElement)A).GetAttribute("ID"));
+            
+            //タグの名前で検索。
+
+            XmlNode B = root.SelectSingleNode("Dataset");
+
+            Console.WriteLine(((XmlElement)B).GetAttribute("attr2"));
+            
+            //Bの最初の子供のリスト
+            XmlNodeList bChildren = B.ChildNodes;
+
+            foreach (XmlNode bchild in bChildren)
+            {
+                Console.WriteLine(((XmlElement)bchild).InnerText);
+            }
+
+            Console.ReadLine();
         }
     }
 }
